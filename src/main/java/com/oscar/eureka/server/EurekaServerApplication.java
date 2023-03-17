7b7aca36d1d4da3b53071904f022af4c626e89b3
orodriguez2015 <oscar.rodriguezbrea@gmail.com>
2023-03-17 23:39:48 +0100

    Servidor Eureka

---
+++ src/main/java/com/oscar/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.oscar.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * Servidor Eureka
+ * @author <a href="mailto:oscar.rodriguezbrea@gmail.com">Óscar Rodríguez</a>
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
